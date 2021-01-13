048559924a7380a12ac013c0cd86bb1db66bc5d8
Dawit Mezemir <dawitmezemir@gmial.com>
2021-01-14 00:20:19 +0100

    Create Patch File

--- client3.py
+++ client3.py
@@ -36,7 +36,7 @@ def getDataPoint(quote):
 	bid_price = float(quote['top_bid']['price'])
 	ask_price = float(quote['top_ask']['price'])
 	price = (bid_price + ask_price)/2 
-	return stock, bid_price, ask_price, price
+	return stock, bid_price, ask_price, price  
 
 def getRatio(price_a, price_b):
 	""" Get ratio of price_a and price_b """
@@ -63,5 +63,5 @@ if __name__ == "__main__":
 			print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 	
 			
-
+		print(prices) 
 		print ("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))   
